13d963e11ec75107d5905a2a596f69c2fe8c4a4e
Expanseman <ethan.blackmail@gmail.com>
2018-09-27 12:54:30 -0700

    added comments; added trials to tests

--- hwk01/encode.py
+++ hwk01/encode.py
@@ -1,24 +1,38 @@
 def encode(input_string):
 
+	# initialize compressed string
 	output_string = ""
 	
+	# check for empty string	
 	if input_string == "":
 		return output_string
 	
+	# compression
 	else:
 		tmp = input_string[0]
 		count = 0	
 
+		# walk through every letter
 		for i in input_string:
+				
+			# if current char is the same as the previous, add 1 to count
+			# first char is also previous; adding 1 to count will bring it to 1
 			if i == tmp:
 				count += 1
+			
+			# if current char is different from previous, add char and count to final string
+			# reset count to one so that the curr char is counted
 			else:
 				output_string += tmp + str(count)
 				count = 1
+
+			# current char becomes the previous char and loop restarts
 			tmp = i
 
+		# after the loop, final string will be created from previous char and its count 
 		output_string += tmp + str(count)
 		
+		# compares size of input and compressed string; returns the smaller
 		if len(output_string) < len(input_string):
 			return output_string
 		else:
--- hwk01/test_encode.py
+++ hwk01/test_encode.py
@@ -8,18 +8,22 @@ def test_non_repeating_string():
 	# non-repeating strings will always return the input string 
 	assert encode("a") == "a"
 	assert encode("abc") == "abc"
+	assert encode("aba") == "aba"
 
 def test_compression():
 	assert encode("aabbbccccddddd") == "a2b3c4d5"
 	assert encode("zzzzzyyyyxxxww") == "z5y4x3w2"
+	assert encode("abbbabbbbabbbbba") == "a1b3a1b4a1b5a1"
 
 def test_edges_of_compression():
 	assert encode("abbbbbbc") == "a1b6c1"
 	assert encode("aabbbbcc") == "a2b4c2"
-	assert encode("aaabbccc") == "a3b2c3"
+	assert encode("aaaab") == "a4b1"
+	assert encode("abbbb") == "a1b4"
 
 def test_case_of_chars():
 	assert encode("aAaaaaa") == "a1A1a5"
+	assert encode("AaAAAAA") == "A1a1A5"
 
 def test_size_of_compression():
 	assert encode("aa") == "aa"
