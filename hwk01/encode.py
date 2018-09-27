27fe889bd9b5b347f1a44eaaddf54bf0d5107736
Expanseman <ethan.blackmail@gmail.com>
2018-09-27 10:44:03 -0700

    Basic compression is working

--- hwk01/encode.py
+++ hwk01/encode.py
@@ -1,2 +1,21 @@
 def encode(input_string):
-	return input_string
+
+	output_string = ""
+	
+	if input_string == "":
+		return output_string
+	
+	else:
+		tmp = input_string[0]
+		count = 0	
+
+		for i in input_string:
+			if i == tmp:
+				count = count + 1
+			else:
+				output_string = output_string + tmp + str(count)
+				count = 1
+			tmp = i
+		output_string = output_string + tmp + str(count)
+
+		return output_string
--- hwk01/test_encode.py
+++ hwk01/test_encode.py
@@ -4,5 +4,10 @@ from encode import encode
 def test_empty_string():
 	assert encode("") == ""
 
-def test_single_char_string():
-	assert encode(input_string) == input_string
+def test_non_repeating_string():
+	assert encode("a") == "a1"
+	assert encode("zzz") == "z3"
+
+def test_solution():
+	assert encode("aabbbccccddddd") == "a2b3c4d5"
+	assert encode("zzzzzyyyyxxxww") == "z5y4x3w2"
